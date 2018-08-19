5150d9dab83f6ac6b203c248f82f6695c983d8f0
yigalang <aykaykangel@gmail.com>
2018-08-19 14:03:25 +0300

    first merge

---
+++ app/src/main/java/com/example/minesweeper/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.minesweeper;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+
+public class MainActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
