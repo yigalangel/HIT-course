c88e8738ea61100bb65e92f2f6d72e41cdd4d27f
Nir and Inbal <Nir and Inbal@DESKTOP-KLU0J3P>
2018-08-21 21:40:13 +0300

    startPage2

--- app/src/main/java/com/example/minesweeper/MainActivity.java
+++ app/src/main/java/com/example/minesweeper/MainActivity.java
@@ -4,17 +4,34 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final ImageView soundBtn = findViewById(R.id.sound);
+        soundBtn.setTag(R.drawable.sound);
+        soundBtn.setOnClickListener(new View.OnClickListener() {
+        public void onClick(View v) {
+            if (Integer.parseInt(soundBtn.getTag().toString()) == R.drawable.mute) {
+                soundBtn.setBackgroundResource(R.drawable.sound);
+                soundBtn.setScaleType(ImageView.ScaleType.FIT_END);
+                soundBtn.setTag(R.drawable.sound);
+            }
+            else if (Integer.parseInt(soundBtn.getTag().toString()) == R.drawable.sound)
+            {
+                soundBtn.setBackgroundResource(R.drawable.mute);
+                soundBtn.setScaleType(ImageView.ScaleType.FIT_END);
+                soundBtn.setTag(R.drawable.mute);
+            }
+        }
+    });
     }
 }
