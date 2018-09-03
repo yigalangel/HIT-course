23673635cb8a179fe6ce844e29c7ed5d77eefa86
Nir and Inbal <Nir and Inbal@DESKTOP-KLU0J3P>
2018-09-03 22:04:06 +0300

    start screen

--- app/src/main/java/com/example/minesweeper/MainActivity.java
+++ app/src/main/java/com/example/minesweeper/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.minesweeper;
 
 import android.app.Activity;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,19 +18,24 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final ImageView soundBtn = findViewById(R.id.sound);
-        soundBtn.setTag(R.drawable.sound);
+
+        ImageView image_start=findViewById(R.id.image_start);
+        AnimationDrawable animationDrawable=(AnimationDrawable)image_start.getDrawable();
+        animationDrawable.start();
+
+        soundBtn.setTag(R.drawable.voicon);
         soundBtn.setOnClickListener(new View.OnClickListener() {
         public void onClick(View v) {
-            if (Integer.parseInt(soundBtn.getTag().toString()) == R.drawable.mute) {
-                soundBtn.setBackgroundResource(R.drawable.sound);
+            if (Integer.parseInt(soundBtn.getTag().toString()) == R.drawable.voicoff) {
+                soundBtn.setBackgroundResource(R.drawable.voicon);
                 soundBtn.setScaleType(ImageView.ScaleType.FIT_END);
-                soundBtn.setTag(R.drawable.sound);
+                soundBtn.setTag(R.drawable.voicon);
             }
-            else if (Integer.parseInt(soundBtn.getTag().toString()) == R.drawable.sound)
+            else if (Integer.parseInt(soundBtn.getTag().toString()) == R.drawable.voicon)
             {
-                soundBtn.setBackgroundResource(R.drawable.mute);
+                soundBtn.setBackgroundResource(R.drawable.voicoff);
                 soundBtn.setScaleType(ImageView.ScaleType.FIT_END);
-                soundBtn.setTag(R.drawable.mute);
+                soundBtn.setTag(R.drawable.voicoff);
             }
         }
     });
